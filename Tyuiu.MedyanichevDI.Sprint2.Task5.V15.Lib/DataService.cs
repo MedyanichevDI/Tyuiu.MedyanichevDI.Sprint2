6e449c6c0e5fc9f82a1c8c3bcf85c6cb83bcc8e5
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-09-29 17:44:31 +0500

    Task5

---
+++ Tyuiu.MedyanichevDI.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MedyanichevDI.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            float ost = 0;
+            float z = (k % 7.0f);
+            
+            string vi = "";
+
+            switch(z)
+            {
+                case 1:
+                    vi = "Понедельник";
+                    break;
+                case 2:
+                    vi = "Вторник";
+                    break;
+                case 3:
+                    vi = "Среда";
+                    break;
+                case 4:
+                    vi = "Четверг";
+                    break;
+                case 5:
+                    vi = "Пятниц";
+                    break;
+                case 6:
+                    vi = "Суббота";
+                    break;
+                case 0:
+                    vi = "Воскресенье";
+                    break;
+                default:
+                    throw new ArgumentException($"День должен быть от 1 до 365. Значение {k}");
+            }
+            return vi;
+        }
+    }
+}
---
+++ Tyuiu.MedyanichevDI.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Tyuiu.MedyanichevDI.Sprint2.Task5.V15.Lib;
+
+namespace Tyuiu.MedyanichevDI.Sprint2.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Медяничев Д.И. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор swich                                                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Дано целое число k (1 <= k <= 365). Определить, каким днем недели        *");
+            Console.WriteLine("*(понедельником, вторником, …, субботой или воскресеньем) является k-й    *");
+            Console.WriteLine("* день не високосного года, в котором 1 января понедельник.               *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("                                                                          *");
+            Console.WriteLine("***************************************************************************");
+
+            //string k;
+
+            Console.WriteLine("Введите значение предложение X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if ((x >= 1) || (x <= 365))
+            {
+                Console.WriteLine("Этот день "+ds.FindDayName(x));
+            }
+            else {
+                Console.WriteLine("Введено не верное значение");
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
